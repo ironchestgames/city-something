1ae8c6b77cdea16f7c8c04c9df2f9575658ab8a6
Fredrik Järnbröst <contact@fredrikvestin.com>
2017-03-20 11:00:52 +0100

    first commit

---
+++ src/js/splashScene.js
@@ -0,0 +1,19 @@
+
+var gameScene = {
+  name: 'gameScene',
+  create: function (sceneParams) {
+    this.container = new PIXI.Container()
+    global.baseStage.addChild(this.container)
+  },
+  destroy: function () {
+    this.container.destroy()
+  },
+  update: function () {
+
+  },
+  draw: function () {
+    global.renderer.render(this.container)
+  },
+}
+
+module.exports = gameScene
